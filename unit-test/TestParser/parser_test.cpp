7e4c85bd745ad9dd8e2603010a18de68f8401df0
AndrewBorisov81 <ibobojohn81@gmail.com>
2023-04-07 14:05:20 +0300

    Update parser_test

--- unit-test/TestParser/parser_test.cpp
+++ unit-test/TestParser/parser_test.cpp
@@ -2,20 +2,38 @@
 
 #include <string>
 #include <queue>
+#include <memory>
 
 #include <gtest/gtest.h>
 
+class ParserTest: public ::testing::Test {
+protected:
+    void SetUp() override {
+         m_parser = std::make_unique<Parser>();
+    }
 
-TEST(ParserTest, parseAlgorithm) {
+    void TearDown() override {
+        m_parser.reset();
+    }
+    std::unique_ptr<Parser> m_parser;
+};
+
+TEST_F(ParserTest, parseAlgorithmSuccess) {
     const std::string parseStr{"5;a"};
     std::queue<std::string> expected;
     expected.push("5");
     expected.push("a");
 
-    Parser parser;
     std::queue<std::string> test;
-    parser.parseAlgorithm(parseStr, std::string(";"), test);
+    m_parser->parseAlgorithm(parseStr, std::string(";"), test);
 
-    EXPECT_EQ(test.size(), expected.size());
-    EXPECT_EQ(test, expected);
+    auto matchContainers = [](const std::queue<std::string>& test, const std::queue<std::string>& expected) {
+        EXPECT_EQ(test.size(), expected.size());
+        EXPECT_EQ(test, expected);  
+    };
+
+    matchContainers(test, expected);
+
+    /*EXPECT_EQ(test.size(), expected.size());
+    EXPECT_EQ(test, expected);*/
 }
