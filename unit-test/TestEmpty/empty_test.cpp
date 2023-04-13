a6b2d0749c9068db7d20123e459b040caf8e3b9c
AndrewBorisov81 <ibobojohn81@gmail.com>
2023-04-13 18:29:12 +0300

    Update empty_test

--- unit-test/TestEmpty/empty_test.cpp
+++ unit-test/TestEmpty/empty_test.cpp
@@ -1,8 +1,69 @@
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
-using ::testing::AtLeast; 
+using ::testing::AtLeast;
 
-TEST(EmptyTest, CanDrawSomething) {
-  bool stop = true;
+class MyClass {
+public:
+  MyClass(int value) : value_(value) {}
+  int GetValue() const { return value_; }
+private:
+  int value_;
+};
+
+class MyTestFixture : public testing::Test {
+protected:
+  virtual void SetUp() {
+    // SetUp code goes here
+    my_class_ = new MyClass(42);
+  }
+
+  virtual void TearDown() {
+    // TearDown code goes here
+    delete my_class_;
+  }
+
+  // Any variables you declare here can be accessed
+  // by all tests in the fixture.
+  MyClass* my_class_;
+};
+
+TEST_F(MyTestFixture, Test1) {
+  EXPECT_EQ(my_class_->GetValue(), 42);
 }
+
+TEST_F(MyTestFixture, Test2) {
+  my_class_->GetValue(); // This will fail the test.
+  FAIL() << "Test not implemented yet";
+}
+
+TEST(MyTestSimple, Test3) {
+  MyClass myClass(10);
+  EXPECT_EQ(myClass.GetValue(), 10);
+}
+
+
+
+
+/*class MyEmptyClass: public ::testing::Test {
+  public:
+    void SetUp() override 
+    {  
+      bool stop1 = true; 
+    }
+    
+    void TearDown() override 
+    {
+      bool stop2 = true;
+    }
+  protected:
+
+};
+
+TEST(EmptyTest, createEmptyTest) {
+  bool stop3 = true;
+}
+
+TEST_F(MyEmptyClass, createEmptyTestFrame) {
+  bool stop4 = true;
+}*/
