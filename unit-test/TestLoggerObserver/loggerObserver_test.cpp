6f0c45885f64941f104007aa32b52c646724a735
AndrewBorisov81 <ibobojohn81@gmail.com>
2023-04-15 06:41:52 +0300

    Update LoggerObserver_test

--- unit-test/TestLoggerObserver/loggerObserver_test.cpp
+++ unit-test/TestLoggerObserver/loggerObserver_test.cpp
@@ -1,11 +1,14 @@
 #include "LoggerObserver.h"
 #include "ILogger.h"
+#include "ISubject.h"
 
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
 #include <memory>
 #include <vector>
+
+using ::testing::AtLeast;
 
 class Cell {
 public:
@@ -26,25 +29,24 @@ private:
 class MockShipsLogger: public ILogger {
 public:
   MOCK_METHOD(void, update, (const std::vector<std::shared_ptr<Cell>>& board,
-    const std::vector<std::shared_ptr<Ship>>& ships), (override));
+      const std::vector<std::shared_ptr<Ship>>& ships), (override));
 };
 
 class MockModelSubject: public ISubject {
 public:
-  //virtual void attach(IObserver* observer) = 0;
-    /*virtual void detach(IObserver* observer) = 0;
-    virtual void notify(const std::vector<std::shared_ptr<Cell>>& board,
-        const std::vector<std::shared_ptr<Ship>>& ships, int rows, int columns) = 0;*/
     MOCK_METHOD(void, attach, (IObserver*), (override));
+    MOCK_METHOD(void, detach, (IObserver*), (override));
+    MOCK_METHOD(void, notify, (const std::vector<std::shared_ptr<Cell>>& board,
+        const std::vector<std::shared_ptr<Ship>>& ships, int rows, int columns), (override));
 };
 
 class TestLoggerObserver : public ::testing::Test {
 public:
     void SetUp() override 
     {   
-        m_shipsLogger = std::make_unique<MockShipsLogger>();
+        //m_shipsLogger = std::make_unique<MockShipsLogger>();
        // m_modelSubject = std::make_unique<MockModelSubject>();
-        m_loggerObserver = std::make_unique<LoggerObserver>();
+        //m_loggerObserver = std::make_unique<LoggerObserver>();
     }
     
     void TearDown() override 
@@ -52,11 +54,21 @@ public:
 
     }
 protected:
-    std::unique_ptr<LoggerObserver> m_loggerObserver;
-    std::unique_ptr<MockShipsLogger> m_shipsLogger;
-    std::unique_ptr<MockModelSubject> m_modelSubject; 
+    //std::unique_ptr<LoggerObserver> m_loggerObserver;
+    //std::unique_ptr<MockShipsLogger> m_shipsLogger;
+    //std::unique_ptr<MockModelSubject> m_modelSubject; 
 };
 
-TEST_F(TestLoggerObserver, createTestLoggerObserver) {
-
+TEST(TestLoggerObserverSimple, createTestLoggerObserver) {
+    std::vector<std::shared_ptr<Cell>> cells;
+    cells.push_back(std::make_shared<Cell>());
+    std::vector<std::shared_ptr<Ship>> ships;
+    ships.push_back(std::make_shared<Ship>());
+    std::unique_ptr<MockShipsLogger> shipsLogger = std::make_unique<MockShipsLogger>();
+    EXPECT_CALL(*shipsLogger, update(cells, ships))                  
+      .Times(AtLeast(1));
+    std::unique_ptr<MockModelSubject> modelSubject = std::make_unique<MockModelSubject>();
+     EXPECT_CALL(*modelSubject, notify(cells, ships, 10, 10))                  
+      .Times(AtLeast(1));
+    auto loggerObserver = std::make_unique<LoggerObserver>(modelSubject, shipsLogger);
 }
